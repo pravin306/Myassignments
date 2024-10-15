496607bb2297bec8308356ae05c3e8d229521168
pravin306 <praving98m@gmail.com>
2024-10-15 13:14:56 +0530

    Add files via upload
    day1

---
+++ week1/findoddnumber.java
@@ -0,0 +1,19 @@
+package day2.week1;
+
+public class findoddnumber{
+	
+
+public static void main(String[] args) {
+	
+	System.out.println("The odd number are");
+	for(int i=0; i<=10; i++)
+	{
+		if(i%2==1)
+			
+			System.out.println(i);
+	}
+	
+	
+	
+}
+}
